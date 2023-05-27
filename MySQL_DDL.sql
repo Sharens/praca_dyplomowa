ce68be0a7b7dabf0553a6f1311b20ab3653faa7a
Sharens <xarens19@gmail.com>
2023-05-28 00:44:21 +0200

    ładowanie danych

--- MySQL_DDL.sql
+++ MySQL_DDL.sql
@@ -5,7 +5,7 @@
 CREATE DATABASE praca_dyplomowa
 
 ----------------------------------------------------------------------
---Tworzenie tabeli z outputem dla wyszukiwań choroby wieńcowej
+-- Tworzenie tabeli z outputem dla wyszukiwań choroby wieńcowej
 ----------------------------------------------------------------------
 CREATE TABLE STG_DIS_Choroba_Wiencowa (
 	DATA  date,
@@ -14,3 +14,10 @@ CREATE TABLE STG_DIS_Choroba_Wiencowa (
     TOTAL integer,
     isPartial varchar(10)
 );
+
+----------------------------------------------------------------------
+-- Ładowanie danych do bazy danych
+----------------------------------------------------------------------
+LOAD DATA INFILE "C:\Users\lenovoo\Downloads\output.csv"
+INTO TABLE stg_dis_choroba_wiencowa
+FIELDS TERMINATED BY ','
